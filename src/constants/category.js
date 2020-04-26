2e6a1ba14db7b571679f7cacbec32b58cb3a31f2
Seong Kwan Roh <seongkwan.roh@gmail.com>
2020-04-27 00:18:13 +0900

    Category 상수파일 생성

---
+++ src/constants/category.js
@@ -0,0 +1,9 @@
+const categories = [
+    {path: '/art', name: 'Art'},
+    {path: '/design', name: 'Design'},
+    {path: '/fashion', name: 'Fashion'},
+    {path: '/music', name: 'Music'},
+    {path: '/story', name: 'Story'},
+]
+
+export default categories;
