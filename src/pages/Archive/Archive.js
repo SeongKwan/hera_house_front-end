badd0e6bc7827f2b5342c4cdba5472d75be51ad0
Seong Kwan Roh <seongkwan.roh@gmail.com>
2020-04-27 08:06:29 +0900

    TopScroll 버튼 생성

--- src/pages/Archive/Archive.js
+++ src/pages/Archive/Archive.js
@@ -10,6 +10,7 @@ import classNames from 'classnames/bind';
 import categories from '../../constants/category';
 import PostList from '../../components/PostList/PostList';
 import Post from '../../components/Post/Post';
+import { IoIosArrowUp } from "react-icons/io";
 
 const cx = classNames.bind(styles);
 const cn = {
@@ -19,14 +20,38 @@ const cn = {
 
 @withRouter
 class Archive extends Component {
+    state = { isSeen: false }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this._showTopScrollButton)
+    }
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this._showTopScrollButton)
+    }
+
+    _showTopScrollButton = () => {
+        let { scrollY } = window,
+            { scrollHeight } = this.mainContainer;
+        let touchDown = ((scrollY / scrollHeight) > 0.3) || scrollY > 2000;
+
+        if (touchDown && !this.state.isSeen) {
+            this.setState({isSeen: true});
+        } else if (!touchDown && this.state.isSeen) {
+            this.setState({isSeen: false});
+        }
+    }
+
     _onClickBrandLogo = () => {
         this.props.history.push('/');
+    }
+
+    _onClickTopScrollButton = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 
     render() {
         let { path, params } = this.props.match;
         let isDetailPage = window.location.pathname.split('/').length > 3 ? true : false;
-        
         return (
             <div className={cx('Archive')}>
                 <header className={cx('header')}>
@@ -41,7 +66,7 @@ class Archive extends Component {
                             })}
                         </nav>
                     </aside>
-                    <main className={cx('main')}>
+                    <main className={cx('main')} ref={ref => this.mainContainer = ref}>
                         <Switch>
                             <Route exact path={`${path}/:category`}>
                                 <PostList />
@@ -50,9 +75,16 @@ class Archive extends Component {
                                 <Post />
                             </Route>
                         </Switch>
+                        {
+                            this.state.isSeen && 
+                            <div className={cx('wrapper-button-top-scroll')}>
+                                <div className={cx('wrapper-icon')} onClick={this._onClickTopScrollButton}>
+                                    <IoIosArrowUp className={cx('icon')} />
+                                </div>
+                            </div>
+                        }
                     </main>
                 </div>
-
             </div>
         );
     }
