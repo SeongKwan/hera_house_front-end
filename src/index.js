b03ea8967e47a607549b6880ad7871f19e6873b8
Seong Kwan Roh <seongkwan.roh@gmail.com>
2020-04-27 00:17:56 +0900

    MobX 적용

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './styles/base.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { configure } from 'mobx';
+import { Provider } from 'mobx-react';
+import stores from './stores';
+
+configure({ 
+    enforceActions: "observed"
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider {...stores}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
