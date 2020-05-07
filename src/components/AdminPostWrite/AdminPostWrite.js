6806d7c1dff926e3fea71f582d2c4af39ec2099b
Seong Kwan Roh <seongkwan.roh@gmail.com>
2020-05-07 19:00:07 +0900

    test editor on mobile

--- src/components/AdminPostWrite/AdminPostWrite.js
+++ src/components/AdminPostWrite/AdminPostWrite.js
@@ -3,7 +3,8 @@ import { withRouter } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import styles from './AdminPostWrite.module.scss';
 import classNames from 'classnames/bind';
-import Editor from '../Editor/Editor';
+// import Editor from '../Editor/Editor';
+import TestEditor from '../TestEditor/TestEditor';
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +15,8 @@ class AdminPostWrite extends Component {
     render() {
         return (
             <div className={cx('AdminPostWrite')}>
-                <Editor type={this.props.type} />
+                <TestEditor type={this.props.type} />
+                {/* <Editor type={this.props.type} /> */}
             </div>
         );
     }
---
+++ src/components/TestEditor/optionsForMobile.js
@@ -0,0 +1,46 @@
+const options = {
+    lang: 'ko-KR',
+    height: '300',
+    dialogsFade: true,
+    dialogsInBody: true,
+    tabDisable: true,
+    fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Helvetica', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana', 'NanumPenScript', 'Sunflower'],
+    fontNamesIgnoreCheck: ['NanumPenScript', 'Sunflower'],
+    placeholder: '어떤 글을 쓰고 싶으세요?',
+    styleTags: [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ],
+    lineHeights: ['0.2', '0.4', '0.6', '0.8', '1.0', '1.2', '1.4', '1.5', '2.0', '3.0'],
+    fontSizes: ['8', '9', '10', '11', '12', '13', '14', '16', '18', '20', '24', '28', '36', '48', '64', '72', '84', '96'],
+    
+    toolbar: [
+        ['misc', ['undo', 'redo']],
+        ['fontName', ['fontname']],
+        ['fontSize', ['fontsize', 'fontsizeunit']],
+        ['font', ['color', 'bold', 'underline', 'strikethrough', 'clear']],
+        ['para', ['style', 'paragraph', 'height']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview']]
+    ],
+    popover: {
+        image: [
+            ['image', ['resizeFull', 'resizeHalf', 'resizeQuarter', 'resizeNone']],
+            ['float', ['floatLeft', 'floatRight', 'floatNone']],
+            ['remove', ['removeMedia']]
+        ],
+        link: [
+            ['link', ['linkDialogShow', 'unlink']]
+        ],
+        table: [
+            ['add', ['addRowDown', 'addRowUp', 'addColLeft', 'addColRight']],
+            ['delete', ['deleteRow', 'deleteCol', 'deleteTable']],
+        ],
+        air: [
+            ['color', ['color']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['para', ['ul', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture']]
+        ]
+    },
+}
+
+export default options;
