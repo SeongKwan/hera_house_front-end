0e55ae4c58e0a8a412622283ca0b7776ee5d69e3
Seong Kwan Roh <seongkwan.roh@gmail.com>
2020-05-07 21:26:39 +0900

    fix

--- src/components/Editor/optionsForMobile.js
+++ src/components/Editor/optionsForMobile.js
@@ -4,20 +4,9 @@ const options = {
     dialogsFade: true,
     dialogsInBody: true,
     tabDisable: true,
-    fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Helvetica', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana', 'NanumPenScript', 'Sunflower'],
-    fontNamesIgnoreCheck: ['NanumPenScript', 'Sunflower'],
-    placeholder: '어떤 글을 쓰고 싶으세요?',
-    styleTags: [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ],
-    lineHeights: ['0.2', '0.4', '0.6', '0.8', '1.0', '1.2', '1.4', '1.5', '2.0', '3.0'],
-    fontSizes: ['8', '9', '10', '11', '12', '13', '14', '16', '18', '20', '24', '28', '36', '48', '64', '72', '84', '96'],
     
     toolbar: [
         ['misc', ['undo', 'redo']],
-        ['fontName', ['fontname']],
-        ['fontSize', ['fontsize', 'fontsizeunit']],
-        ['font', ['color', 'bold', 'underline', 'strikethrough', 'clear']],
-        ['para', ['style', 'paragraph', 'height']],
-        ['insert', ['link', 'picture', 'video']],
         ['view', ['fullscreen', 'codeview']]
     ],
     popover: {
