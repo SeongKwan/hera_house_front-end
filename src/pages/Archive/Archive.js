7dda0862249822e26cb3a6d0a21e2f1668702182
Seong Kwan Roh <seongkwan.roh@gmail.com>
2020-05-16 20:44:27 +0900

    restore fn

--- src/pages/Archive/Archive.js
+++ src/pages/Archive/Archive.js
@@ -62,6 +62,14 @@ class Archive extends Component {
         });
     }
 
+    _handleClickOnListItemInMenu = () => {
+        setTimeout(()=> {
+            this.setState({
+                openMenu: !this.state.openMenu
+            });
+        }, 500)
+    }
+
     _handleOnClickSns = (e, type) => {
         return window.open(sns[type], '_blank');
     }
