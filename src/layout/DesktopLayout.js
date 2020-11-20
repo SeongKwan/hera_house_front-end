77177919b9262bd644c0e02889735c551b22a5f7
sk.roh <seongkwan.roh@gmail.com>
2020-11-20 16:33:45 +0900

    add: home, layout css effect

--- src/layout/DesktopLayout.js
+++ src/layout/DesktopLayout.js
@@ -10,46 +10,54 @@ const cx = classNames.bind(styles);
 @inject('categoryStore', 'loginStore')
 @observer
 class DesktopLayout extends Component {
+    componentDidMount() {
+        this.props.categoryStore.loadCategories();
+    }
     render() {
-        console.log(JSON.parse(JSON.stringify(this.props.mainCategories)));
-        return (
-            <div className={cx('DesktopLayout')}>
-                <header>
-                    <div className={cx('concept-title')}>
-                        <Link to={'/'}><span>BLACK ENDING</span></Link>
-                    </div>
-                    <nav className={cx('home-nav')}>
-                        <ul>
-                            <li className={cx('nav-item', 'nav-item--herakim')}><Link to={`/about`}>HERA KIM</Link></li>
-                            <li className={cx('nav-item', 'nav-item--projects')}><Link to={`/projects`}>PROJECTS</Link></li>
-                            <li className={cx('nav-item', 'nav-item--archives')}>
-                                <Link to={`/archives`}>ARCHIVES</Link>
-                                <ul className={cx('sub-nav')}>
-                                    {
-                                        this.props.mainCategories.map((category, i) => {
-                                            return <li key={`sub-nav-item-${i}`} className={cx('sub-nav-item', `sub-nav-item--${category.name}`)}>
-                                                <Link to={`/archives/${category}`}>{category.name}</Link>
-                                                {
-                                                    category.name === "Work" &&
-                                                    <ul className={cx('work-sub')}>
-                                                        {category.subCategories.map((categoryWithSub, i) => {
-                                                            return <li key={`work-sub-item-${i}`} className={cx('work-sub-item')}>
-                                                                <Link to={`/archives/work/${categoryWithSub.name}`}>{categoryWithSub.name}</Link>
-                                                            </li>
-                                                        })}
-                                                    </ul>
-                                                }
-                                            </li>
-                                        })
-                                    }
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-                {this.props.children}
-            </div>
-        );
+        let mainCategories = this.props.categoryStore.registry || [];
+        if (mainCategories.length <= 0) {
+            return <div></div>
+        } else
+            return (
+                <div className={cx('DesktopLayout')}>
+                    <header>
+                        <div className={cx('concept-title')}>
+                            <Link to={'/'}><span>BLACK ENDING</span></Link>
+                        </div>
+                        <nav className={cx('home-nav')}>
+                            <ul>
+                                <li className={cx('nav-item', 'nav-item--herakim')}><Link to={`/about`}>HERA KIM</Link></li>
+                                <li className={cx('nav-item', 'nav-item--projects')}><Link to={`/projects`}>PROJECTS</Link></li>
+                                <li className={cx('nav-item', 'nav-item--archives')}>
+                                    <Link to={`/archives`}>ARCHIVES</Link>
+                                    <ul className={cx('sub-nav')}>
+                                        {
+                                            mainCategories.map((category, i) => {
+                                                return <li key={`sub-nav-item-${i}`} className={cx('sub-nav-item', `sub-nav-item--${category.name}`)}>
+                                                    <Link to={`/archives/${category}`}>{category.name}</Link>
+                                                    {
+                                                        category.name === "Work" &&
+                                                        <ul className={cx('work-sub')}>
+                                                            {category.subCategories.map((categoryWithSub, i) => {
+                                                                return <li key={`work-sub-item-${i}`} className={cx('work-sub-item')}>
+                                                                    <Link to={`/archives/work/${categoryWithSub.name}`}>{categoryWithSub.name}</Link>
+                                                                </li>
+                                                            })}
+                                                        </ul>
+                                                    }
+                                                </li>
+                                            })
+                                        }
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <main>
+                        {this.props.children}
+                    </main>
+                </div>
+            );
     }
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,56 +14,50 @@ const cx = classNames.bind(styles);
 @inject('categoryStore', 'loginStore')
 @observer
 class Home extends Component {
-    componentDidMount() {
-        this.props.categoryStore.loadCategories();
-    }
+
     render() {
-        let mainCategories = this.props.categoryStore.registry;
-        if (mainCategories.length > 0) {
 
-            return (
-                <div className={cx('Home')}>
-                    <Helmet>
-                        <title>HR ARCHIVE - HOME</title>
-                        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                        <link rel="canonical" href="http://hr-archive.com" />
-                        {/* 검색엔진 단어 */}
-                        <meta name="Keywords" content="fashion, brand, design, art, music" />
-                        {/* <meta name="Description" content="브랜드 디자이너 에일의 다양한 분야에 관한 그 만의 글이 모여있는 곳" /> */}
-                        {/* 사이트 제목 */}
-                        <meta http-equiv="Title" content="Hera's Archive" />
-                        {/* 사이트 주제 */}
-                        <meta http-equiv="Subject" content="영감을 얻어가기 좋은 곳" />
-                        {/* 자작사 */}
-                        <meta http-equiv="Publisher" content="HhEeRrAa" />
-                        {/* 메일 주소 */}
-                        {/* <meta http-equiv="Reply-To" content="we@naver.com" /> 
-                        <meta http-equiv="Email" content="naver@naver.com" /> */}
-                    </Helmet>
-                    <DesktopLayout mainCategories={mainCategories}>
-                        <div className={cx('identity')}>
-                            <p>Republic Korea based</p>
-                            <p>Design & Styling with</p>
-                            <p>Personally Concept Projects by</p>
-                            <p>Design director Hera Kim</p>
+        return (
+            <div className={cx('Home')}>
+                <Helmet>
+                    <title>HR ARCHIVE - HOME</title>
+                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                    <link rel="canonical" href="http://hr-archive.com" />
+                    {/* 검색엔진 단어 */}
+                    <meta name="Keywords" content="fashion, brand, design, art, music" />
+                    {/* <meta name="Description" content="브랜드 디자이너 에일의 다양한 분야에 관한 그 만의 글이 모여있는 곳" /> */}
+                    {/* 사이트 제목 */}
+                    <meta http-equiv="Title" content="Hera's Archive" />
+                    {/* 사이트 주제 */}
+                    <meta http-equiv="Subject" content="영감을 얻어가기 좋은 곳" />
+                    {/* 자작사 */}
+                    <meta http-equiv="Publisher" content="HhEeRrAa" />
+                    {/* 메일 주소 */}
+                    {/* <meta http-equiv="Reply-To" content="we@naver.com" /> 
+                    <meta http-equiv="Email" content="naver@naver.com" /> */}
+                </Helmet>
+                <DesktopLayout>
+                    <div className={cx('identity')}>
+                        <p>Republic Korea based</p>
+                        <p>Design & Styling with</p>
+                        <p>Personally Concept Projects by</p>
+                        <p>Design director Hera Kim</p>
 
-                            <span className={cx('site-name')}>HR ARCHIVE</span>
-                        </div>
-                        <div className={cx('concept-point')}>
-                            <p>When all colors</p>
-                            <p>are combined,</p>
-                            <p>the end is clearly BLACK</p>
-                        </div>
-                        <div className={cx('sns')}>
-                            <div className={cx('youtube')}>Youtube</div>
-                            <div className={cx('instagram')}>Instagram</div>
-                        </div>
-                    </DesktopLayout>
-                </div>
-            );
-        } else {
-            return <div className={cx('Home')}></div>
-        }
+                        <span className={cx('site-name')}>HR ARCHIVE</span>
+                    </div>
+                    <div className={cx('concept-point')}>
+                        <p>When all colors</p>
+                        <p>are combined,</p>
+                        <p>the end is clearly BLACK</p>
+                    </div>
+                    <div className={cx('sns')}>
+                        <div className={cx('youtube')}>Youtube</div>
+                        <div className={cx('instagram')}>Instagram</div>
+                    </div>
+                </DesktopLayout>
+            </div>
+        ); return <div className={cx('Home')}></div>
+
     }
 }
 
