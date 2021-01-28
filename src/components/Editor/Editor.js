7bf25d99fa3a9ebaf5622f920db7373fc250bc18
sk.roh <seongkwan.roh@gmail.com>
2021-01-29 06:27:42 +0900

    not video yet

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -96,7 +96,7 @@ const CustomToolbar = () => (
         <span className="ql-formats">
             <button className="ql-link" />
             <button className="ql-image" />
-            <button className="ql-video" />
+            {/* <button className="ql-video" /> */}
         </span>
         <span className="ql-formats">
             <button className="ql-clean" />
