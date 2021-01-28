ccd266ac1a5d437573755f21b1e27408bceda74d
sk.roh <seongkwan.roh@gmail.com>
2021-01-28 23:16:39 +0900

    fix archives visible on not published

--- src/pages/Archives/Archives.js
+++ src/pages/Archives/Archives.js
@@ -3,9 +3,7 @@ import { withRouter } from 'react-router';
 import { inject, observer } from 'mobx-react';
 import styles from './Archives.module.scss';
 import classNames from 'classnames/bind';
-import {
-    Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import DesktopLayout from '../../layout/DesktopLayout';
 import staticUrl from '../../constants/staticUrl';
@@ -22,8 +20,12 @@ class Archives extends Component {
     }
 
     componentDidUpdate(prevProps) {
-        if ((prevProps.location.pathname.split('/')[2] !== this.props.location.pathname.split('/')[2])
-            || (prevProps.location.pathname.split('/')[3] !== this.props.location.pathname.split('/')[3])) {
+        if (
+            prevProps.location.pathname.split('/')[2] !==
+                this.props.location.pathname.split('/')[2] ||
+            prevProps.location.pathname.split('/')[3] !==
+                this.props.location.pathname.split('/')[3]
+        ) {
             this._initialize();
         }
     }
@@ -37,7 +39,7 @@ class Archives extends Component {
         const category = this.props.location.pathname.split('/')[2];
         const subCategory = this.props.location.pathname.split('/')[3];
         this.props.postStore.loadFilteredPosts({ type, category, subCategory });
-    }
+    };
 
     render() {
         const subCategory = this.props.location.pathname.split('/')[2];
@@ -49,55 +51,92 @@ class Archives extends Component {
             <div className={cx('Archives')}>
                 <Helmet>
                     <title>HR ARCHIVE - ARCHIVES</title>
-                    <link rel="canonical" href={`http://hr-archive.com/archives`} />
-                    <meta http-equiv="Title" content={`HR ARCHIVE - ARCHIVES`} />
-                    <meta name="Keywords" content="fashion, brand, design, art, music" />
+                    <link
+                        rel="canonical"
+                        href={`http://hr-archive.com/archives`}
+                    />
+                    <meta
+                        http-equiv="Title"
+                        content={`HR ARCHIVE - ARCHIVES`}
+                    />
+                    <meta
+                        name="Keywords"
+                        content="fashion, brand, design, art, music"
+                    />
                     <meta name="Description" content="HR Archive Archive" />
                 </Helmet>
                 <DesktopLayout fadeIn>
                     <div className={cx('flex-box')}>
                         <div className={cx('breadcrumb')}>
                             <span>ARCHIVES</span>
-                            {
-                                subCategory !== undefined ?
-                                    <span>{` : ${subCategory}`}</span>
-                                    : <span></span>
-                            }
-                            {
-                                aCategory !== undefined ?
-                                    <span>{` : ${aCategory}`}</span>
-                                    : <span></span>
-                            }
-                            {
-                                title !== undefined ?
-                                    <span>{` : ${title}`}</span>
-                                    : <span></span>
-                            }
+                            {subCategory !== undefined ? (
+                                <span>{` : ${subCategory}`}</span>
+                            ) : (
+                                <span></span>
+                            )}
+                            {aCategory !== undefined ? (
+                                <span>{` : ${aCategory}`}</span>
+                            ) : (
+                                <span></span>
+                            )}
+                            {title !== undefined ? (
+                                <span>{` : ${title}`}</span>
+                            ) : (
+                                <span></span>
+                            )}
                         </div>
-                        
+
                         <div className={cx('grid-container')}>
                             <ul className={cx('grid')}>
-                                {
-                                    posts.length >= 0 && !isLoading ?
+                                {posts.length >= 0 && !isLoading ? (
                                     posts.map((post, i) => {
-                                        return <li key={`projects-card-${post._id}}`} className={cx('list-item')}>
-                                            <Link to={`/viewer?category=${post.type}_${post.category}_${post.subCategory}&title=${post.title}&id=${post._id}`}>
-                                                <div className={cx('image')}>
-                                                    <img src={`${staticUrl}/${post.thumbnail}`} alt="post thumbnail" />
-                                                </div>
-                                                <div className={cx('list-item-title')}>{post.title}</div>
-                                            </Link>
-                                        </li>
+                                        const { isPublished } = post;
+                                        if (isPublished)
+                                            return (
+                                                <li
+                                                    key={`projects-card-${post._id}}`}
+                                                    className={cx('list-item')}
+                                                >
+                                                    <Link
+                                                        to={`/viewer?category=${post.type}_${post.category}_${post.subCategory}&title=${post.title}&id=${post._id}`}
+                                                    >
+                                                        <div
+                                                            className={cx(
+                                                                'image',
+                                                            )}
+                                                        >
+                                                            <img
+                                                                src={`${staticUrl}/${post.thumbnail}`}
+                                                                alt="post thumbnail"
+                                                            />
+                                                        </div>
+                                                        <div
+                                                            className={cx(
+                                                                'list-item-title',
+                                                            )}
+                                                        >
+                                                            {post.title}
+                                                        </div>
+                                                    </Link>
+                                                </li>
+                                            );
+                                        else {
+                                            return false;
+                                        }
                                     })
-                                    : <div className={cx('loader-container')}><Loader /></div>
-                                }
-                                {
-                                    postsLength === 0 && !isLoading && <div className={cx('no-results')}>Nothings in here, yet.</div>
-                                }
+                                ) : (
+                                    <div className={cx('loader-container')}>
+                                        <Loader />
+                                    </div>
+                                )}
+                                {postsLength === 0 && !isLoading && (
+                                    <div className={cx('no-results')}>
+                                        Nothings in here, yet.
+                                    </div>
+                                )}
                             </ul>
                         </div>
                     </div>
-
                 </DesktopLayout>
             </div>
         );
